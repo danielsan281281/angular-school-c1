89d8cc9e3f4ccf242f96af1f1b7e95e9540cde88
danielsan281281 <danisancho@gmail.com>
2022-05-29 07:26:41 +0200

    15. Bindings bidireccionales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public myName:string = 'Dani';
+
+  onSayHello(message){
+    console.log(message);
+  }
 }
---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core'
+
+@Component({
+    selector: 'custom-hello',
+    templateUrl: './hello.component.html',
+    styleUrls: [
+        './hello.component.css'
+    ]
+})
+export class HelloComponent{
+    @Input() name:string;
+    @Output() sayHello:EventEmitter<string> = new EventEmitter<string>();
+
+    public namesList:Array<string> = [];
+    public inputName:string = "John";
+
+    constructor(){
+    }
+
+    onNameClick(){
+        this.sayHello.emit('Hello!');
+    }
+
+    addName(){
+        this.namesList.push(this.inputName);
+        this.inputName = "";
+        console.log(this.namesList);
+    }
+
+    updateInputName(event:any){
+        this.inputName = event.target.value;
+        console.log(this.inputName);
+    }
+}
